71049a249db5314fa0c377a0c51c71746055a12f
FKenan <kenanf734@gmail.com>
2025-06-15 16:56:35 +0300

    AXIOS kütüphanesi ile veri çekme işlemini tek bir yerden yönetme

---
+++ src/api/apiClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000/";
+
+// methods ile yöntemleri tanımladık. products ile de bu yöntemleri kullanarak ürünlerle ilgili işlemleri tanımladık.
+// requests ile de bu ürün işlemlerini dışarıya açtık. Böylece diğer dosyalarda bu işlemleri kullanabileceğiz.
+// Bu yapı sayesinde API isteklerini merkezi bir yerden yönetmiş olduk.
+const methods = {
+  get: (url) => axios.get(url).then((response) => response.data),
+  post: (url, body) => axios.post(url, body).then((response) => response.data),
+  put: (url, body) => axios.put(url, body).then((response) => response.data),
+  delete: (url) => axios.delete(url).then((response) => response.data),
+};
+
+const products = {
+  list: () => methods.get("products"), // baseurl üstüne eklenir
+  details: (id) => methods.get(`products/${id}`),
+};
+
+const requests = {
+  products,
+};
+
+export default requests;
