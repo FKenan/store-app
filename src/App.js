097985f643f4d4499e274889ee743b5eb721dba8
FKenan <kenanf734@gmail.com>
2025-06-16 21:42:37 +0300

    Cart Contetx => cart verilerini globalde tutma

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ServerErrorPage from "./pages/errors/ServerError";
 import NotFoundPage from "./pages/errors/NotFound";
 import { useEffect } from "react";
 import requests from "./api/apiClient";
+import { useCartContext } from "./context/CartContext";
 
 export const router = createBrowserRouter([
   {
@@ -44,6 +45,8 @@ export const router = createBrowserRouter([
 ]);
 
 function App() {
+  const { setCart } = useCartContext();
+
   useEffect(() => {
     requests.cart
       .get()
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { CartContextProvider } from "./context/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </StrictMode>
 );
 // strictMode kullanırsak componentler iki kez render edilir.hata ayıklaması içinn
